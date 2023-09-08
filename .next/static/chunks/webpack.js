d11c95e6aebfd28a820cc1f5099d2f4765acd90a
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-08 12:41:16 +0200

    LD adding the next folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7a25f26b8ba1b6d6"; }
+/******/ 		__webpack_require__.h = function() { return "6e32ed7f8c30603d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "22a55f5d8e953851"; }
+/******/ 	__webpack_require__.h = function() { return "3fa60e1f4e4ef26a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
