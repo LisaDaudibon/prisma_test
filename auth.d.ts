07121b564d71ef4d2dc9069f2cac22a70d96239f
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-15 22:43:08 +0200

    LD adding the new fetch

--- auth.d.ts
+++ auth.d.ts
@@ -2,39 +2,29 @@ import 'next-auth/jwt'
 import { DefaultSession } from 'next-auth'
 
 declare module 'next-auth/jwt' {
-  interface JWT {
+interface JWT {
+  discriminator: string
+  global_name: string
+  verified: boolean
+  mfa_enabled: boolean
+  banner: string
+  playerName: string
+  access_token:string
+  }
+}
+
+declare module 'next-auth' {
+interface Session {
+  user: {
     discriminator: string
     global_name: string
     verified: boolean
     mfa_enabled: boolean
     banner: string
     playerName: string
-    discordGuild: DiscordGuild[]
-  }
-  interface DiscordGuild {
-    discordGuildName: string
-    icon: string
-    ownerId: string
-    approximate_number_count: integer
-    approximate_presence_count: integer
-    description: string
-    guildName: string
-    // jwt: JWT[]
-  }
-}
-
-declare module 'next-auth' {
-  interface Session {
-    user: {
-      discriminator: string
-      global_name: string
-      verified: boolean
-      mfa_enabled: boolean
-      banner: string
-      playerName: string
-      discordGuild: DiscordGuild[]
+    access_token: string
     } & DefaultSession['user']
-  }
+}
 
   interface User {
     discriminator: string
@@ -43,16 +33,5 @@ declare module 'next-auth' {
     mfa_enabled: boolean
     banner: string
     playerName: string
-    discordGuild: DiscordGuild[]
-  }
-  interface DiscordGuild {
-    discordGuildName: string
-    icon: string
-    owner_id: string
-    approximate_number_count: integer
-    approximate_presence_count: integer
-    description: string
-    guildName: string
-    // user: User[]
   }
 }
