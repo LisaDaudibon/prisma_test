f0bee68df3309b6473dabccc7494d836a582a730
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-15 11:47:17 +0200

    LD a little modifying of prisma

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "16aab30da4cba3a2"; }
+/******/ 	__webpack_require__.h = function() { return "dffbadfba374ef4c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
