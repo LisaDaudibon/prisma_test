8cab66ebe885eabc3ac2718a61c2b8c87e607075
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-20 17:59:40 +0200

    LD adding the element to next-config and adding everything to the package.json

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     appDir: true,
     serverComponentsExternalPackages: ["@prisma/client"]
   },
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 }
 
 module.exports = nextConfig
